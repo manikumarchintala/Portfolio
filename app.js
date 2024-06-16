3e9dc2c4412da70fb0c626ae05402a2e17a1f38a
Mani Kumar Chintala <85386219+manikumarchintala@users.noreply.github.com>
2024-06-16 12:58:26 -0500

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const http=require('http');
-http.createServer(function (req,res){
-	res.write("on the way to being a dev");
-	res.end();
-}).listen(3000);
-console.log("server started on port 3000");
