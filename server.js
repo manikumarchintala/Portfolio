7a6611ddf9d44493fbfaae78f67059e840190242
Mani Kumar Chintala <85386219+manikumarchintala@users.noreply.github.com>
2024-06-17 22:29:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-  //Hamburgermenu
-  const nav = document.querySelector(".nav");
-  const menu = document.querySelector(".menu");
-  const close = document.querySelector(".close");
-  const body = document.querySelector(".body");
-  menu.addEventListener("click", () => {
-    nav.classList.add("nav--visible");
-    menu.classList.add("menu--hide");
-    close.classList.add("close--show");
-    body.classList.add("body--no-scroll");
-  });
-  close.addEventListener("click", () => {
-    nav.classList.remove("nav--visible");
-    menu.classList.remove("menu--hide");
-    close.classList.remove("close--show");
-    body.classList.remove("body--no-scroll");
-  });
-  //for contact form
-  const contactForm = document.querySelector(".contact-form");
-  let name = document.getElementById("name");
-  let email = document.getElementById("email");
-  let subject = document.getElementById("subject");
-  let message = document.getElementById("message");
-  let warningEmail = document.querySelector(".warning--email");
-  let tem = email.value;
-  function validateEmail(tem) {
-    let validRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    return validRegex.test(tem);
-  }
+const express = require("express");
+const server = require("http");
+require("dotenv").config();
+const app = express();
+const nodemailer = require("nodemailer");
 
-  email.addEventListener("input", () => {
-    const isValidEmail = validateEmail(email.value);
-  });
+const PORT = process.env.PORT || 6500;
 
-  contactForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (name.value && email.value && subject.value && message.value) {
-      let formData = {
-        name: name.value,
-        email: email.value,
-        subject: subject.value,
-        message: message.value,
-      };
-      let xhr = new XMLHttpRequest();
-      xhr.open("POST", "/");
-      xhr.setRequestHeader("content-type", "application/json");
-      xhr.onload = function () {
-        console.log(xhr.responseText);
-        if (xhr.responseText == "sucess") {
-          name.value = "";
-          email.value = "";
-          subject.value = "";
-          message.value = "";
-        }
-      };
-      xhr.send(JSON.stringify(formData));
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.user,
+      pass: process.env.pass,
+    },
+  });
+  const mailOptions = {
+    from: req.body.email,
+    to: process.env.user,
+    subject: `Message  from ${req.body.email}:${req.body.subject}`,
+    text: req.body.message,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("error");
+      res.send("error");
+    } else {
+      console.log(`Email sent : + ${info.response}`);
+      res.send("sucess");
     }
   });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
