a5b6ea540857e2355090b59fd1cb775bc7689c60
Mani Kumar Chintala <85386219+manikumarchintala@users.noreply.github.com>
2024-06-17 22:01:38 -0500

    Add files via upload

---
+++ public/scripts/main.js
@@ -0,0 +1,72 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //Hamburgermenu
+  const nav = document.querySelector(".nav");
+  const menu = document.querySelector(".menu");
+  const close = document.querySelector(".close");
+  const body = document.querySelector(".body");
+  menu.addEventListener("click", () => {
+    nav.classList.add("nav--visible");
+    menu.classList.add("menu--hide");
+    close.classList.add("close--show");
+    body.classList.add("body--no-scroll");
+  });
+  close.addEventListener("click", () => {
+    nav.classList.remove("nav--visible");
+    menu.classList.remove("menu--hide");
+    close.classList.remove("close--show");
+    body.classList.remove("body--no-scroll");
+  });
+  //for contact form
+  const contactForm = document.querySelector(".contact-form");
+  let name = document.getElementById("name");
+  let email = document.getElementById("email");
+  let subject = document.getElementById("subject");
+  let message = document.getElementById("message");
+  let warningEmail = document.querySelector(".warning--email");
+  let tem = email.value;
+  function validateEmail(tem) {
+    let validRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return validRegex.test(tem);
+  }
+
+  email.addEventListener("input", () => {
+    const isValidEmail = validateEmail(email.value);
+  });
+
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const isValidEmail = validateEmail(email.value);
+    if (!isValidEmail) {
+      warningEmail.innerText = "* please enter a valid email!!";
+    } else {
+      warningEmail.innerText = "";
+    }
+    if (
+      name.value &&
+      isValidEmail &&
+      subject.value &&
+      message.value &&
+      wrong()
+    ) {
+      let formData = {
+        name: name.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value,
+      };
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "/");
+      xhr.setRequestHeader("content-type", "application/json");
+      xhr.onload = function () {
+        console.log(xhr.responseText);
+        if (xhr.responseText == "sucess") {
+          name.value = "";
+          email.value = "";
+          subject.value = "";
+          message.value = "";
+        }
+      };
+      xhr.send(JSON.stringify(formData));
+    }
+  });
+});
